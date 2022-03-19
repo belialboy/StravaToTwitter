c9c6550cbd02c47ea1102f15bbf645998b447a63
EC2 Default User <ec2-user@ip-172-31-18-217.eu-west-1.compute.internal>
2022-03-19 13:09:21 +0000

    Trying to resolve the cyclic dependency

--- src/register.py
+++ src/register.py
@@ -2,6 +2,7 @@
 import json
 import os
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -10,10 +11,18 @@ def lambda_handler(event, context):
 
     logging.info("Underpants")
     logging.info(event)
+    
+    cloudformation = boto3.client("cloudformation")
+    stack = cloudformation.describe_stacks(StackName=os.environ['stackID'])
+    redirectUrl = None
+    for output in stack['Stacks'][0]['Outputs']:
+        if output['OutputKey'] == "RegisterSuccessUrl":
+            redirectUrl = output['OutputValue']
+            
     returnable = {
         "statusCode": 301,
         "headers": {
-           "headers": {"Location": "https://www.strava.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT}&response_type=code&scope=activity:read_all".format(CLIENT_ID=os.environ['stravaClientId'],REDIRECT=os.environ['stravaRedirect']), }
+           "headers": {"Location": "https://www.strava.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT}&response_type=code&scope=activity:read_all".format(CLIENT_ID=os.environ['stravaClientId'],REDIRECT=redirectUrl), }
         },
         "body": ""
     }
