a065d915a0e9bc23249c9bb443b4459b5a68a42a
EC2 Default User <ec2-user@ip-172-31-27-76.eu-west-1.compute.internal>
2022-04-05 07:32:06 +0000

    Debugging

--- test/proxy_test.py
+++ test/proxy_test.py
@@ -9,7 +9,9 @@ class TestProxy(unittest.TestCase):
     def test_register_redirect(self):
         stream = os.popen('sam local invoke ProxyLambdaFunction -e test/payloads/register_test.json 2> /dev/null')
         output = stream.read()
-        stream.close()
+        stream.close
+        print("START{}END".format(output))
+        self.assertIsNotNone(output)
         expectation={"statusCode": 301, "headers": {"Location": "https://www.strava.com/oauth/authorize?client_id=pStravaClientId&redirect_uri=https://abcde123456.execute-api.eu-west-1.amazonaws.com/registersuccess/&response_type=code&scope=activity:read_all"}, "body": ""}
 
         self.assertEqual(json.loads(output),expectation)
